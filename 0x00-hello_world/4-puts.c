aa9a3c98e76ae171a9a391715b9b84db5825fc3c
ifwmuli <mathewmulity@gmail.com>
2023-02-16 07:58:19 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
  * short description
- * main :a program that uses puts to print a lin
+ * main - a program that uses puts to print a lin
  *  Return: 0 (Success)
- *  
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+puts("\"Programming is like building a multilingual puzzle");return (0);
 }
