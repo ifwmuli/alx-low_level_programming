04fa77235d726566ac6340c243b0dd11a81dd1ba
ifwmuli <mathewmulity@gmail.com>
2023-02-16 10:51:39 -0800

    weuh

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,8 +3,7 @@
  * main- A program that prints  the size of various computer types
  * Return 0 (Success)
  */
-
-int main(void)
+main(void)
 {
 
 printf("Size of a char: %i bytes\n", sizeof(char));
