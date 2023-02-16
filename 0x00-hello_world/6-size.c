b7c735089614e6c894bbc742ff7bd53715785f7e
ifwmuli <mathewmulity@gmail.com>
2023-02-16 09:54:51 -0800

    main

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,11 @@ int b;
 long int c;
 long long int d;
 float f;
-printf("The size of a char: %lu bytes\n", (unsigned long) sizeof(a));
-printf("The size of an int: %lu bytes\n", (unsigned long) sizeof(b));
-printf("The size of a long int: %lu bytes\n", (unsigned long) sizeof(c));
-printf("The size of a long long int: %lu bytes\n", (unsigned long) sizeof(d));
-printf("The size of a float: %lu bytes\n", (unsigned long) sizeof(f));
+printf("The size of a char:%lu bytes\n", (unsigned long) sizeof(a));
+printf("The size of an int:%lu bytes\n", (unsigned long) sizeof(b));
+printf("The size of a long int:%lu bytes\n", (unsigned long) sizeof(c));
+printf("The size of a long long int:%lu bytes\n", (unsigned long) sizeof(d));
+printf("The size of a float:%lu bytes\n", (unsigned long) sizeof(f));
 return (0);
 }
 
