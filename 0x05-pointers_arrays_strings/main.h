58aff1fcaa4c89a07df112e5423299987e3491db
ifwmuli <mathewmulity@gmail.com>
2023-02-27 11:02:45 -0800

    main

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,4 @@
 #ifndef MAIN_H
-#define MAIN_H
 
 
 
@@ -19,6 +18,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
+int _atoi(char * *dest, char *rcs);
 
 #endif
