d8d9b867347ddf11f12596930625f65b796610dc
ifwmuli <mathewmulity@gmail.com>
2023-02-27 11:25:53 -0800

    strcpy

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,6 +4,10 @@
 
 
 
+
+
+#include <stdlib.h>
+
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -18,3 +22,4 @@ char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
 #endif
+
